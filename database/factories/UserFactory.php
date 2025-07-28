0f657cdeb26145ed2c2f7a474d76c9eacdf41c5b
MKashanButt <kashanbutt566@gmail.com>
2025-07-28 12:39:38 -0400

    update user factory to generate company name as well

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,17 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => fake()
+                ->name(),
+            'email' => fake()
+                ->unique($reset = true)
+                ->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            "company" => fake()
+                ->unique($reset = true)
+                ->company(),
         ];
     }
 
@@ -37,7 +43,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
