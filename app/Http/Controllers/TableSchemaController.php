b58eec56b9a5e6177c6dd46c48878f71d2eb1be3
MKashanButt <kashanbutt566@gmail.com>
2025-07-29 13:33:02 -0400

    finished table schema controller refactoring

--- app/Http/Controllers/TableSchemaController.php
+++ app/Http/Controllers/TableSchemaController.php
@@ -2,88 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ApiResponse;
 use App\Http\Requests\CreateSchemaRequest;
 use App\Models\TableSchema;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Services\TableSchemaService;
+use App\Transformers\TableSchemaTransformer;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Auth;
 
 class TableSchemaController extends Controller
 {
+    use AuthorizesRequests;
+    private TableSchemaService $schemaService;
+    public function __construct(TableSchemaService $schemaService)
+    {
+        $this->schemaService = $schemaService;
+    }
     public function index(): JsonResponse
     {
         try {
-            $schemas = TableSchema::where("user_id", Auth::id())->get();
-
-            return response()->json([
-                "status" => "success",
-                "data" => $schemas
-            ], 201);
+            $this->authorize('viewAny',  TableSchema::class);
+            $schemas = $this->schemaService->index();
+            return ApiResponse::success(data: TableSchemaTransformer::collection($schemas));
         } catch (\Exception $e) {
-            return response()->json([
-                "status" => "error",
-                "data" => [
-                    "message" => "Unexpected error",
-                    "error" => $e->getMessage()
-                ]
-            ], 500);
+            return ApiResponse::error(message: "Unexpected error", errors: $e->getMessage(), statusCode: 500);
         }
     }
-
-    public function show(int $id): JsonResponse
+    public function show(TableSchema $tableSchema): JsonResponse
     {
         try {
-            $schema = TableSchema::where("id", $id)
-                ->where("user_id", Auth::id())
-                ->firstOrFail();
-
-            return response()->json([
-                "status" => "success",
-                "data" => [
-                    "slug" => $schema->slug,
-                    "schema" => $schema->schema,
-                    "user_id" => $schema->user_id
-                ]
-            ]);
-        } catch (ModelNotFoundException $e) {
-            return response()->json([
-                "status" => "error",
-                "data" => [
-                    "message" => "Schema not found or access denied."
-                ]
-            ], 404);
+            $this->authorize('view',  $tableSchema);
+            $schema = $this->schemaService->show($tableSchema);
+            return ApiResponse::success(data: (new TableSchemaTransformer($schema)->transform()));
         } catch (\Exception $e) {
-            return response()->json([
-                "status" => "error",
-                "data" => [
-                    "message" => "Unexpected error",
-                    "error" => $e->getMessage()
-                ]
-            ], 500);
+            return ApiResponse::error(message: "Unexpected error", errors: $e->getMessage(), statusCode: 500);
         }
     }
     public function store(CreateSchemaRequest $request): JsonResponse
     {
         try {
+            $this->authorize('create',  TableSchema::class);
             $validatedRequest = $request->validated();
-            $validatedRequest["user_id"] = Auth::id();
-
-            TableSchema::create($validatedRequest);
-
-            return response()->json([
-                "status" => "success",
-                "data" => [
-                    "message" => "Schema was Created"
-                ]
-            ], 201);
+            $this->schemaService->store($validatedRequest);
+            return ApiResponse::success(message: "Schema was Created", statusCode: 201);
         } catch (\Exception $e) {
-            return response()->json([
-                "status" => "error",
-                "data" => [
-                    "message" => "Schema Creation Failed",
-                    "error" => $e->getMessage()
-                ]
-            ], 500);
+            return ApiResponse::error(message: "Unexpected error", errors: $e->getMessage(), statusCode: 500);
         }
     }
 }
