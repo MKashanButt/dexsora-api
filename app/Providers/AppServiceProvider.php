b24f1a47c69f9dd4c5502a17d1e7a533a0421bea
MKashanButt <kashanbutt566@gmail.com>
2025-07-29 15:15:33 -0400

    added schema default length to 191 characters

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\Data;
 use App\Models\TableSchema;
 use App\Policies\DataPolicy;
 use App\Policies\TableSchemaPolicy;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -32,6 +33,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
