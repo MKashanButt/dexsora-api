935999b8cd86cbc85c07f3ffe6bbced0ea5d736a
MKashanButt <kashanbutt566@gmail.com>
2025-07-29 13:40:44 -0400

    added data store and update request

---
+++ app/Http/Requests/StoreDataRequest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreDataRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "data" => ["required", "array"],
+            "schema_id" => ["required", "integer", "exists:schemas,id"],
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function messages(): array
+    {
+        return [
+            "data.required" => "data is required",
+            "data.json" => "data should be a valid json",
+            "schema_id.required" => "schema_id is required",
+            "schema_id.integer" => "schema_id should be a valid integer",
+            "schema_id.exists" => "schema_id does not exsists",
+        ];
+    }
+
+    /**
+     * Get the validation attributes that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function attributes(): array
+    {
+        return [
+            "schema_id" => "Schema"
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => 'error',
+            'message' => 'Validation errors',
+            'errors' => $validator->errors()
+        ], 422));
+    }
+}
