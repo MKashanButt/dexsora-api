ec7fc99a3d14b0f16dff8ab39b104e9e39b7b2f6
MKashanButt <kashanbutt566@gmail.com>
2025-07-29 13:41:24 -0400

    registered policies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,23 @@
 
 namespace App\Providers;
 
+use App\Models\Data;
+use App\Models\TableSchema;
+use App\Policies\DataPolicy;
+use App\Policies\TableSchemaPolicy;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Registering policies
+     * @var array
+     */
+    protected $policies = [
+        Data::class => DataPolicy::class,
+        TableSchema::class => TableSchemaPolicy::class,
+    ];
+
     /**
      * Register any application services.
      */
